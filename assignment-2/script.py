16e803ce26bb4363905963a11705fd7349c6ba0b
Ravi Ailani <rvbugs0@gmail.com>
2023-02-26 20:36:48 -0600

    Programming finish - report pending

--- assignment-2/script.py
+++ assignment-2/script.py
@@ -12,6 +12,9 @@ import itertools
 
 if __name__ == "__main__":
 
+    # set this to False to make test cases / training set non-random
+    randomness_allowed = True
+
     iris = load_iris()
     X = iris.data
     Y = iris.target
@@ -23,8 +26,9 @@ if __name__ == "__main__":
     for c in classes:
         c_indices = np.where(Y == c)[0].tolist()
 
-        c_indices = sorted(c_indices, key=(
-            lambda x: x - random.randint(-1000,1000 )))
+        if (randomness_allowed):
+            c_indices = sorted(c_indices, key=(
+                lambda x: x - random.randint(-1000, 1000)))
         all_test_indices = all_test_indices + c_indices[:n]
 
     test_X = X[all_test_indices]
@@ -37,62 +41,57 @@ if __name__ == "__main__":
 
 # data is now evenly split according to 1.1
 
+    print("\nExpected target values ",test_Y,"\n")
+
     # sepal length / width
     print("MODEL-1: Logistic Regression - features # sepal length / width")
     model1 = MulticlassClassification(learning_rate=0.05, max_epochs=1000)
     model1.fit(train_X[:, :2], train_Y)
-    print(model1.predict(test_X[:, :2]))
-    print(accuracy_score(test_Y, model1.predict(test_X[:, :2])))
+    print("Predictions",model1.predict(test_X[:, :2]))
+    print("Accuracy - model1:",accuracy_score(test_Y, model1.predict(test_X[:, :2])),"\n")
+    
 
     print("MODEL-1-LDA: features # sepal length / width")
     model1_LDA = LDA(n_components=2)
     model1_LDA.fit(train_X[:, :2], train_Y)
     predictions = model1_LDA.predict(test_X[:, :2], transformed=False)
-    print(predictions)
-    print(accuracy_score(test_Y, predictions))
+    print("Predictions",predictions)
+    print("Accuracy - model1_LDA:",accuracy_score(test_Y, predictions),"\n")
 
     # petal length / width
     print("MODEL-2: Logistic Regression - features # petal length / width")
     model2 = MulticlassClassification(learning_rate=0.05, max_epochs=1000)
     model2.fit(train_X[:, -2:], train_Y)
-    print(model2.predict(test_X[:, -2:]))
-    print(accuracy_score(test_Y, model2.predict(test_X[:, -2:])))
+    print("Predictions",model2.predict(test_X[:, -2:]))
+    print("Accuracy - model2:",accuracy_score(test_Y, model2.predict(test_X[:, -2:])),"\n")
 
     # petal length / width
     print("MODEL-2-LDA: features # petal length / width")
     model2_LDA = LDA(n_components=2)
     model2_LDA.fit(train_X[:, -2:], train_Y)
     predictions = model2_LDA.predict(test_X[:, -2:], transformed=False)
-    print(predictions)
-    print(accuracy_score(test_Y, predictions))
+    print("Predictions",predictions)
+    print("Accuracy - model2_LDA:",accuracy_score(test_Y, predictions),"\n")
 
     # all features
-    print("Model-3: Logistic Regression-  # all features")
+    print("Model-3: Logistic Regression-  all features")
     model3 = MulticlassClassification(learning_rate=0.05, max_epochs=1000)
     model3.fit(train_X, train_Y)
-    print(model3.predict(test_X))
-    print(accuracy_score(test_Y, model3.predict(test_X)))
+    print("Predictions",model3.predict(test_X))
+    print("Accuracy - model3:",accuracy_score(test_Y, model3.predict(test_X)),"\n")
 
-    print("Model-3: LDA-  # all features")
+    print("Model-3: LDA - all features")
     model3_LDA = LDA(n_components=2)
     model3_LDA.fit(train_X, train_Y)
     predictions = model3_LDA.predict(test_X, transformed=False)
-    print(predictions)
-    print(accuracy_score(test_Y, predictions))
+    print("Predictions",predictions)
+    print("Accuracy - model3_LDA:",accuracy_score(test_Y, predictions),"\n")
 
     # Plotting Decision Regions
     gs = gridspec.GridSpec(2, 3)
     fig = plt.figure(figsize=(16, 8))
     value = 1.5
     width = 0.75
-
-    # fig = plot_decision_regions(X=test_X[:, -3:],
-
-    # filler_feature_values={
-    #     2: value},
-    # filler_feature_ranges={
-    #     2: width},
-    #                             y=test_Y, clf=model2, legend=2)
 
     for clf, lab, grd, test_set in zip([model1, model1_LDA, model2, model2_LDA, model3, model3_LDA],
                                        ['Logistic Regression - Sepal Length/Width', 'LDA - Sepal Length/Width',
@@ -103,18 +102,19 @@ if __name__ == "__main__":
                                         (1, 1), (0, 2), (1, 2)],
 
                                        [test_X[:, :2], model1_LDA.transform(test_X[:, :2]),
-                                        test_X[:, -2:], model2_LDA.transform(test_X[:, -2:]),
+                                        test_X[:, -
+                                               2:], model2_LDA.transform(test_X[:, -2:]),
                                         test_X, model3_LDA.transform(test_X)
                                         ]
                                        ):
         ax = plt.subplot(gs[grd[0], grd[1]])
         fig = plot_decision_regions(X=test_set, y=test_Y, clf=clf, legend=2,
                                     filler_feature_values={
-                                        2: value,3:value},
+                                        2: value, 3: value},
                                     filler_feature_ranges={
-                                        2: width,3:width},
-                                        feature_index=[0,1]
+                                        2: width, 3: width},
+                                    feature_index=[0, 1]
                                     ),
-        
+
         plt.title(lab)
     plt.show()

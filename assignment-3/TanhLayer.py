c59aaa90ccbae19ff0ac3b42934757326c29d685
Ravi Ailani <rvbugs0@gmail.com>
2023-03-10 14:36:17 -0500

    Some more changes added

--- assignment-3/TanhLayer.py
+++ assignment-3/TanhLayer.py
@@ -20,12 +20,13 @@ class TanhLayer(Layer):
         grad_input = output_gradient * tanh_grad
         return grad_input
 
-tanh_layer_1 = TanhLayer()
-linear_layer_1 = LinearLayer(2, 2)
-tanh_layer_2 = TanhLayer()
-linear_layer_2 = LinearLayer(2, 1)
+# tanh_layer_1 = TanhLayer()
+# linear_layer_1 = LinearLayer(2, 2)
+# tanh_layer_2 = TanhLayer()
+# linear_layer_2 = LinearLayer(2, 1)
 
-model_tanh = NeuralNetwork()
-model_tanh.add_layer(linear_layer_1)
-model_tanh.add_layer(tanh_layer_1)
-model_tanh.add_layer(linear_layer_2
+# model_tanh = NeuralNetwork()
+# model_tanh.add_layer(linear_layer_1)
+# model_tanh.add_layer(tanh_layer_1)
+# model_tanh.add_layer(linear_layer_2)
+
